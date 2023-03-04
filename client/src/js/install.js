4613b2ae6bd7157b4c232a86f00d757db52a8522
Pipoxy <jparke5@hotmail.com>
2023-03-04 01:06:16 -0700

    done

---
+++ client/src/js/install.js
@@ -0,0 +1,27 @@
+const butInstall = document.getElementById("buttonInstall");
+
+window.addEventListener("beforeinstallprompt", (event) => {
+  // stores events
+  window.deferredPrompt = event;
+
+  butInstall.classList.toggle("hidden", false);
+});
+
+butInstall.addEventListener("click", async () => {
+  const promptEvent = window.deferredPrompt;
+
+  if (!promptEvent) {
+    return;
+  }
+
+  promptEvent.prompt();
+
+  window.deferredPrompt = null;
+
+  butInstall.classList.toggle("hidden", true);
+});
+
+window.addEventListener("appinstalled", (event) => {
+  // clears prompt
+  window.deferredPrompt = null;
+});
